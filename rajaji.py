251378ae3b51ab0d60084414c743746bdaf24180
Shsonu33 <189646112+Shsonu33@users.noreply.github.com>
2025-01-11 18:28:00 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7330439352:AAGe4akhUf57yi6wNDRCl44IRoot4ehq3cc")
+BOT_TOKEN=("7898311586:AAGBiCfnO-YPIp8SVjJirmO744qC6NnRATs")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@Itz_sonu_9")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6005957043","6329099426")
+ADMIN_IDS=("6005957043")
 
 
 
